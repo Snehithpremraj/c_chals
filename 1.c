55a385866a9381d78fb642a57d4a750b993e53e7
Snehithpremraj <snehithpremraj@gmail.com>
2021-06-01 01:27:08 +0530

    completed

--- 1.c
+++ 1.c
@@ -2,12 +2,19 @@
 int main()
 {
     char str1[20],str2[20];
-    int l1,l2;
+    int l1=0,l2=0;
     scanf("%s",&str1);
     scanf("%s",&str2);
-    l1=strlen(str1);
-    l2=strlen(str2);
-    if(l1>l2)
+   
+for(int i=0;str1[i]!='\0';i++)
+{
+    l1+=1;
+}
+for(int i=0;str2[i]!='\0';i++)
+{
+    l2+=1;
+}
+if(l1>l2)
     {
         printf("\n First");
     }
