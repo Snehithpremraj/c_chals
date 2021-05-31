484823628f595dc5c9b3ad35de6806ec9a726be9
Snehithpremraj <snehithpremraj@gmail.com>
2021-05-31 21:48:59 +0530

    completed

--- 2.c
+++ 2.c
@@ -1,41 +1,32 @@
 #include<stdio.h>
-int max(int []);
-void sort(int *);
-int main()
-{   int i,n,a[4],m,s=0;
-    printf("Enter a number ");
-    scanf("%d",&n);
-    for(i=0;i<3;i++)
-        scanf("%d",&a[i]);
-    sort(a);
-    for(i=0;i<3;i++)
-        printf("%d",a[i]);
-    m=a[2];
-    printf("\n%d",m);
-    while(n!=0)
-    {   for(i=0;i<3;i++)
-        {if(a[i]>=m)
-            {
-                m=a[i];
-                a[i]=a[i]-1;
-            }
-        s+=m;
-     printf("\n%d",s);   
-        }
-    n--;
+void main()
+{
+int a,b,c,n,x=0;
+scanf("%d %d %d %d",&n,&a,&b,&c);
+for(int i=0;i<n;i++)
+{
+    if(a>=b && a>=c)
+    {    
+     x=x+a;
+    if(a>=1){
+    a=a-1;
     }
-    
-   
+    }
+    else if(b>=a && b>=c)
+    {
+     x=x+b;
+     if(b>=1)
+     {
+     b=b-1;
+     }
+    }   
+    else
+    {
+     x=x+c;
+     if(c>=1)
+{     c=c-1;
 }
-void sort(int *ptr)
-{   int tmp=0;
-    for(int i=0;i<3;i++)
-    {   for(int j=i+1;j<3;j++)
-        if (ptr[i]>ptr[j])
-        {
-            tmp=ptr[i];
-            ptr[i]=ptr[j];
-            ptr[j]=tmp;
-        }
     }
+}
+printf("%d",x);
 }
